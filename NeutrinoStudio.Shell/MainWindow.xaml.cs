8fb167d2468b180aac9d028de1122233f7ff6e3c
Il Harper <afanyiyu@hotmail.com>
2020-02-27 12:08:41 +0800

    feat: MainWindow Layout Changes

--- NeutrinoStudio.Shell/MainWindow.xaml.cs
+++ NeutrinoStudio.Shell/MainWindow.xaml.cs
@@ -18,13 +18,11 @@ namespace NeutrinoStudio.Shell
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            this.MouseLeftButtonDown += (sender, e) => this.DragMove();
         }
     }
 }
