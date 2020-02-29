7c3c2863f7fcc4c3ef369e0e84d3d216f9941253
Il Harper <afanyiyu@hotmail.com>
2020-02-29 15:02:46 +0800

    feat: Add Icon Size Property

--- NeutrinoStudio.Utilities/Controls/Icon.cs
+++ NeutrinoStudio.Utilities/Controls/Icon.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
+using System.Windows.Media;
 
 namespace NeutrinoStudio.Utilities.Controls
 {
@@ -22,24 +23,53 @@ namespace NeutrinoStudio.Utilities.Controls
             typeof(string),
             typeof(Icon),
             new PropertyMetadata(
-                default(string),
+                "Account",
                 TypePropertyChangedCallback));
 
         private static void TypePropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             Icon icon = ((Icon) d);
             if (string.IsNullOrEmpty(icon.Type)) return;
-            if (!(icon._currentChild is null)) icon.RemoveLogicalChild(icon._currentChild);
-            using (FileStream fs = new FileStream(Path.Combine(Environment.CurrentDirectory, $"Assets/Icons/{icon.Type}/{icon.Type}_16x.xaml"), FileMode.Open))
-                icon.AddChild((DependencyObject)XamlReader.Load(fs));
+            using (FileStream fs =
+                new FileStream(
+                    Path.Combine(Environment.CurrentDirectory, $"Assets/Icons/{icon.Type}/{icon.Type}_16x.xaml"),
+                    FileMode.Open))
+            {
+                icon.CurrentChild = XamlReader.Load(fs) as DependencyObject;
+                icon.Content = icon.CurrentChild;
+            }
         }
 
-        private readonly object _currentChild = null;
+        private DependencyObject CurrentChild { get; set; }
 
+        /// <summary>
+        /// The type of icon.
+        /// </summary>
         public string Type
         {
             get => (string) GetValue(TypeProperty);
             set => SetValue(TypeProperty, value);
         }
+
+        public static readonly DependencyProperty SizeProperty = DependencyProperty.Register(
+            "Size", typeof(double), typeof(Icon), new PropertyMetadata((double) 1, SizePropertyChangedCallback));
+
+        private static void SizePropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Icon icon = ((Icon)d);
+            icon.SetValue(RenderTransformProperty,
+                new TransformGroup() {Children = {new ScaleTransform(icon.Size, icon.Size)}});
+        }
+
+        /// <summary>
+        /// The size of icon.
+        /// </summary>
+        public double Size
+        {
+            get => (double) GetValue(SizeProperty);
+            set => SetValue(SizeProperty, value);
+        }
+
+        public new Transform RenderTransform => (Transform)GetValue(RenderTransformProperty);
     }
 }
