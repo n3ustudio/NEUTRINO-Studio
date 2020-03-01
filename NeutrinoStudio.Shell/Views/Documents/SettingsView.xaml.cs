2d5dd107d51c67ad4726d42854379c89b4a3b8f0
Il Harper <afanyiyu@hotmail.com>
2020-03-01 12:28:45 +0800

    feat: Add SettingsView

---
+++ NeutrinoStudio.Shell/Views/Documents/SettingsView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YDock.Interface;
+
+namespace NeutrinoStudio.Shell.Views.Documents
+{
+    /// <summary>
+    /// SettingsView.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingsView : UserControl, IDockSource
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+
+        public IDockControl DockControl { get; set; }
+        public string Header => "设置";
+        public ImageSource Icon => null;
+    }
+}
