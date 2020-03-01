0e87ed02ac1588e42bd0115f1dd0e1c8512cd4f9
Il Harper <afanyiyu@hotmail.com>
2020-03-01 22:49:56 +0800

    feat: Add TaskView

---
+++ NeutrinoStudio.Shell/Views/Docks/TaskView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YDock.Interface;
+
+namespace NeutrinoStudio.Shell.Views.Docks
+{
+    /// <summary>
+    /// TaskView.xaml 的交互逻辑
+    /// </summary>
+    public partial class TaskView : UserControl, IDockSource
+    {
+        public TaskView()
+        {
+            InitializeComponent();
+        }
+
+        public IDockControl DockControl { get; set; }
+        public string Header => "任务";
+        public ImageSource Icon => null;
+    }
+}
