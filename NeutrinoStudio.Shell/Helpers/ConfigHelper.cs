064469c19e14d66652a12b88f0b66e57544c74ff
Il Harper <afanyiyu@hotmail.com>
2020-03-01 12:29:06 +0800

    feat: Add ConfigHelper

---
+++ NeutrinoStudio.Shell/Helpers/ConfigHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeutrinoStudio.Shell.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static string GetAppSetting(string key)
+        {
+            if (ConfigurationManager.AppSettings.AllKeys.Contains(key))
+            {
+                string value = config.AppSettings.Settings[key].Value;
+                return value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static void UpdateAppSettings(string key, string value)
+        {
+            if (ConfigurationManager.AppSettings.AllKeys.Contains(key))
+            {
+                //如果当前节点存在，则更新当前节点
+                config.AppSettings.Settings[key].Value = value;
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            else
+            {
+                Console.WriteLine("当前节点不存在");
+            }
+        }
+    }
+}
