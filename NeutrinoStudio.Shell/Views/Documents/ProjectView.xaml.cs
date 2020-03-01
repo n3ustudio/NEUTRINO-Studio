c1ef8846c82e4914b7dba0c1027f5f0c1201d76c
Il Harper <afanyiyu@hotmail.com>
2020-03-01 15:16:14 +0800

    feat: Project View Changes

--- NeutrinoStudio.Shell/Views/Documents/ProjectView.xaml.cs
+++ NeutrinoStudio.Shell/Views/Documents/ProjectView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,15 +21,23 @@ namespace NeutrinoStudio.Shell.Views.Documents
     /// <summary>
     /// ProjectView.xaml 的交互逻辑
     /// </summary>
-    public partial class ProjectView : UserControl, IDockSource
+    public partial class ProjectView : UserControl, IDockSource, INotifyPropertyChanged
     {
         public ProjectView()
         {
             InitializeComponent();
+
+            DataContext = this;
         }
 
         public IDockControl DockControl { get; set; }
         public string Header => "项目";
         public ImageSource Icon => null;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
