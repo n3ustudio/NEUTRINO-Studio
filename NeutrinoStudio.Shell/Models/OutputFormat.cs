1b948f4ed2107af4f15798befe61bcb8da54a789
Il Harper <afanyiyu@hotmail.com>
2020-03-01 17:27:34 +0800

    feat: Add OutputFormat Helper

---
+++ NeutrinoStudio.Shell/Models/OutputFormat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NeutrinoStudio.Shell.Models
+{
+    public enum OutputFormat
+    {
+        Undefined = 0,
+        Wav = 1,
+        Mp3 = 2,
+        Flac = 3
+    }
+
+    public static class OutputFormatHelper
+    {
+        public static OutputFormatConverter OutputFormatConverter { get; } = new OutputFormatConverter();
+    }
+
+    public class OutputFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value != null && (OutputFormat)value == (OutputFormat)int.Parse(parameter.ToString());
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && !(bool)value) return null;
+            return (OutputFormat)int.Parse(parameter.ToString());
+        }
+    }
+}
