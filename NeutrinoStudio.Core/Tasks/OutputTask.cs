c2025993b6fdd6a3ebef6e8c5bc6ddc23257d917
Il Harper <afanyiyu@hotmail.com>
2020-03-02 23:27:54 +0800

    feat: Add OutputTask & SynthTask

--- NeutrinoStudio.Core/Tasks/OutputTask.cs
+++ NeutrinoStudio.Core/Tasks/OutputTask.cs
@@ -1,22 +1,112 @@
-﻿using System;
+﻿using NeuTask;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Task = NeuTask.Task;
 
 namespace NeutrinoStudio.Core.Tasks
 {
     public sealed class OutputTask : Task
     {
+
+        /// <summary>
+        /// Create a new output task.
+        /// </summary>
+        /// <param name="workingDir">The working dir where there is a musicXMLtoLabel executable file.</param>
+        /// <param name="f0InputDir">F0 file.</param>
+        /// <param name="mgcInPutDir">Mgc file.</param>
+        /// <param name="bapInputDir">Bap file.</param>
+        /// <param name="outputFile">Output file.</param>
+        public OutputTask(
+            string workingDir,
+            string f0InputDir,
+            string mgcInPutDir,
+            string bapInputDir,
+            string outputFile)
+        {
+            _mainProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    Arguments = $" {f0InputDir} {mgcInPutDir} {bapInputDir} -o {outputFile}",
+                    CreateNoWindow = true,
+                    StandardOutputEncoding = Encoding.UTF8,
+                    WorkingDirectory = workingDir,
+                    FileName = Path.Combine(workingDir, "bin\\WORLD.exe"),
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+            Status = TaskStatus.Waiting;
+            Target = outputFile;
+            Percentage = 0;
+            Message = "启动";
+        }
+
+        public string Name { get; } = "合成";
+
+        private readonly Process _mainProcess;
+
         public override void Start()
         {
-            throw new NotImplementedException();
+            if (Status != TaskStatus.Waiting) return;
+            _mainProcess.Exited += MainProcessOnExited;
+            _mainProcess.OutputDataReceived += MainProcessOnOutputDataReceived;
+            _mainProcess.ErrorDataReceived += MainProcessOnErrorDataReceived;
+            _mainProcess.Start();
+            _mainProcess.BeginErrorReadLine();
+            _mainProcess.BeginOutputReadLine();
+            Status = TaskStatus.Running;
+        }
+
+        private void MainProcessOnErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data is null) return;
+            Message = e.Data;
+            Percentage = 1;
+            _mainProcess.Dispose();
+            Status = TaskStatus.Failed;
+        }
+
+        private void MainProcessOnOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data is null) return;
+            if (e.Data.Contains("Synthesis"))
+            {
+                Message = "正在合成";
+                Percentage = 0.5;
+            }
+        }
+
+        private void MainProcessOnExited(object sender, EventArgs e)
+        {
+            _mainProcess.Dispose();
+            Percentage = 1;
+            Message = "完成";
+            Status = TaskStatus.Complete;
         }
 
         public override void Stop()
         {
-            throw new NotImplementedException();
+            if (Status != TaskStatus.Running) return;
+            try
+            {
+                _mainProcess.OutputDataReceived -= MainProcessOnOutputDataReceived;
+                _mainProcess.Kill();
+                _mainProcess.Dispose();
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+
+            Status = TaskStatus.Failed;
         }
     }
 }
--- NeutrinoStudio.Core/Tasks/SynthTask.cs
+++ NeutrinoStudio.Core/Tasks/SynthTask.cs
@@ -1,22 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using NeuTask;
 using Task = NeuTask.Task;
 
 namespace NeutrinoStudio.Core.Tasks
 {
     public sealed class SynthTask : Task
     {
+
+        /// <summary>
+        /// Create a new synth task.
+        /// </summary>
+        /// <param name="workingDir">The working dir where there is a musicXMLtoLabel executable file.</param>
+        /// <param name="fullInputDir">Full lab file.</param>
+        /// <param name="timingOutputDir">Timing lab file.</param>
+        /// <param name="f0OutputDir">F0 file.</param>
+        /// <param name="mgcOutPutDir">Mgc file.</param>
+        /// <param name="bapOutputDir">Bap file.</param>
+        /// <param name="modelDir">Model dir.</param>
+        public SynthTask(
+            string workingDir,
+            string fullInputDir,
+            string timingOutputDir,
+            string f0OutputDir,
+            string mgcOutPutDir,
+            string bapOutputDir,
+            string modelDir)
+        {
+            _mainProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    Arguments = $" {fullInputDir} {timingOutputDir} {f0OutputDir} {mgcOutPutDir} {bapOutputDir} {modelDir}",
+                    CreateNoWindow = true,
+                    StandardOutputEncoding = Encoding.UTF8,
+                    WorkingDirectory = workingDir,
+                    FileName = Path.Combine(workingDir, "bin\\NEUTRINO.exe"),
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+            Status = TaskStatus.Waiting;
+            Target = f0OutputDir;
+            Percentage = 0;
+            Message = "启动";
+        }
+
+        public string Name { get; } = "生成";
+
+        private readonly Process _mainProcess;
+
         public override void Start()
         {
-            throw new NotImplementedException();
+            if (Status != TaskStatus.Waiting) return;
+            _mainProcess.Exited += MainProcessOnExited;
+            _mainProcess.OutputDataReceived += MainProcessOnOutputDataReceived;
+            _mainProcess.ErrorDataReceived += MainProcessOnErrorDataReceived;
+            _mainProcess.Start();
+            _mainProcess.BeginErrorReadLine();
+            _mainProcess.BeginOutputReadLine();
+            Status = TaskStatus.Running;
+        }
+
+        private void MainProcessOnErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data is null) return;
+            Message = e.Data;
+            Percentage = 1;
+            _mainProcess.Dispose();
+            Status = TaskStatus.Failed;
+        }
+
+        private void MainProcessOnOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data is null) return;
+            if (e.Data.Contains("Predict timing"))
+            {
+                Message = "正在生成时间";
+                Percentage = 0.2;
+            }
+            else if (e.Data.Contains("Predict acoustic features"))
+            {
+                Message = "正在生成";
+                Percentage = 0.5;
+            }
+        }
+
+        private void MainProcessOnExited(object sender, EventArgs e)
+        {
+            _mainProcess.Dispose();
+            Percentage = 1;
+            Message = "完成";
+            Status = TaskStatus.Complete;
         }
 
         public override void Stop()
         {
-            throw new NotImplementedException();
+            if (Status != TaskStatus.Running) return;
+            try
+            {
+                _mainProcess.OutputDataReceived -= MainProcessOnOutputDataReceived;
+                _mainProcess.Kill();
+                _mainProcess.Dispose();
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+
+            Status = TaskStatus.Failed;
         }
     }
 }
