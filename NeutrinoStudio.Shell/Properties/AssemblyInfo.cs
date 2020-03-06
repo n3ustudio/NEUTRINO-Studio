9fb3e451cc0d3a5e7f97b394d3f257ae7589d5b5
Il Harper <afanyiyu@hotmail.com>
2020-03-06 13:19:40 +0800

    chore: Version Update

--- NeutrinoStudio.Shell/Properties/AssemblyInfo.cs
+++ NeutrinoStudio.Shell/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: AssemblyVersion("0.1.4.0")]
+[assembly: AssemblyFileVersion("0.1.4.0")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", ConfigFileExtension = "config", Watch = true)]
