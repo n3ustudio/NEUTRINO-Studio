ef9f3246773a725d25b3c3a7c2ace2c9cce5d292
Il Harper <afanyiyu@hotmail.com>
2020-03-11 21:29:49 +0800

    feat: Navigator Class Changes

--- NeutrinoStudio.Shell/ViewModels/Navigator.cs
+++ NeutrinoStudio.Shell/ViewModels/Navigator.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Scrosser.Models;
 
 namespace NeutrinoStudio.Shell.ViewModels
 {
@@ -10,12 +13,39 @@ namespace NeutrinoStudio.Shell.ViewModels
     /// <summary>
     /// The global navigator.
     /// </summary>
-    public class Navigator
+    public class Navigator : INotifyPropertyChanged
     {
 
         #region Static Current
 
         public static Navigator Current { get; set; } = new Navigator();
+
+        #endregion
+
+        #region Scross
+
+        private Scross _scross = new Scross();
+
+        public Scross Scross
+        {
+            get => _scross;
+            set
+            {
+                _scross = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         #endregion
 
