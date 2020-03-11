ac772e917ecab8f048eb1320c8efb33d4000b2a9
Il Harper <afanyiyu@hotmail.com>
2020-03-11 21:12:40 +0800

    feat: Add Global Navigator

---
+++ NeutrinoStudio.Shell/ViewModels/Navigator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeutrinoStudio.Shell.ViewModels
+{
+
+    /// <summary>
+    /// The global navigator.
+    /// </summary>
+    public class Navigator
+    {
+
+        #region Static Current
+
+        public static Navigator Current { get; set; } = new Navigator();
+
+        #endregion
+
+    }
+
+}
