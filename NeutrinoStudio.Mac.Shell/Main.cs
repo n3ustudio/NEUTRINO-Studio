c485e49004ae356b931594b8e02d49f409ac77a7
Il Harper <afanyiyu@hotmail.com>
2020-03-21 16:51:58 +0800

    feat: Add Neutrino.Mac.Shell Project

---
+++ NeutrinoStudio.Mac.Shell/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace NeutrinoStudio.Mac.Shell
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
