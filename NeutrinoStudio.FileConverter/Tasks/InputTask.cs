2d8c5f6db046680cac176a6b7a461e714222211a
Il Harper <afanyiyu@hotmail.com>
2020-03-23 14:50:47 +0800

    feat: Add InputTask in FileConverter

---
+++ NeutrinoStudio.FileConverter/Tasks/InputTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task = NeuTask.Task;
+
+namespace NeutrinoStudio.FileConverter.Tasks
+{
+
+    class InputTask : Task
+    {
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
